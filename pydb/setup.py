76f6c377db33e63be78326ccd7467c5a2347d0f8
hacchuu0119 <kudokana33@gmail.com>
2019-11-05 20:05:14 +0900

    Missing version update

--- pydb/setup.py
+++ pydb/setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="pydb",
-    version="0.1.0",
+    version="0.1.1",
     packages=setuptools.find_packages(),
     install_requires=[
         "sqlalchemy"
